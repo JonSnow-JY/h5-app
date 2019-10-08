4b64937ff6f44ef45eb1075585fb5f2dccf15140
JonSnow <1571298579@qq.com>
2019-10-08 15:46:32 +0800

    不想再做这么繁琐的事了，直接做项目

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
+const isProduction = process.env.NODE_ENV === "production";
+
+const cdn = {
+  css: ["xxx.css", "sss.js"],
+  js: ["xxxx.js", "sss.js"]
+};
+
 module.exports = {
   chainWebpack: config => {
     // config
